a925b6bf89e1aec8ed4cbb83f718209e86a64f90
ZanderVBita <zanderbernardino@gmail.com>
2024-03-12 19:33:34 -0300

    Add atributo área
    Coloquei um atributo chamado área na classe red ballon

--- red_balloon.py
+++ red_balloon.py
@@ -4,10 +4,16 @@ from enemy import enemy
 class red_balloon(enemy):
 
     def __init__(self):
-        super(red_balloon, self).__init__()
+        super().__init__()
+        self.name = 'Red Ballon'
         image = pygame.image.load("images/enemy.png")
         image.convert()
         image = pygame.transform.scale(image, (50, 50))
         self.image = image
         self.health = 1
         self.speed = 1
+        self.area = 1
+        
+    
+    
+
